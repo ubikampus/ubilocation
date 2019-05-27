bfe229076ca35facff060808fec03783fe775a89
je-l <je-l@users.noreply.github.com>
2019-05-27 16:47:24 +0300

    Add tests for environment.ts and typeUtil.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,13 +1,10 @@
 import * as t from 'io-ts';
 import process from 'process';
+import { unsafeDecode } from './typeUtil';
 
 const EnvDecoder = t.union([t.literal('development'), t.literal('production')]);
 type Env = t.TypeOf<typeof EnvDecoder>;
 
 export const currentEnv = (): Env => {
-  if (process.env.NODE_ENV === 'production') {
-    return 'production';
-  }
-
-  return 'development';
+  return unsafeDecode(EnvDecoder, process.env.NODE_ENV || '');
 };
