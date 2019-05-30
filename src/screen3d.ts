7a8980cbf37ad7f929ee3577dfac518bb992855c
Matti Riekkinen <matti.riekkinen@icloud.com>
2019-05-30 13:11:08 +0300

    Prettify code formatting

--- src/screen3d.ts
+++ src/screen3d.ts
@@ -7,17 +7,21 @@ export const drawScreen3d = () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   document.body.appendChild(canvas);
+
   // Load the 3D engine
   const engine = new BABYLON.Engine(canvas, true, {
     preserveDrawingBuffer: true,
     stencil: true,
   });
+
   // call the createScene function
   const scene = createScene(canvas, engine);
+
   // run the render loop
   engine.runRenderLoop(() => {
     scene.render();
   });
+
   // the canvas/window resize event handler
   window.addEventListener('resize', () => {
     engine.resize();
@@ -41,14 +45,17 @@ const createScene = (canvas: any, engine: any): any => {
 
   // Change to ortographic projection
   // camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
+
   // Attach the camera to the canvas
   camera.attachControl(canvas, true);
+
   // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
   const light = new BABYLON.HemisphericLight(
     'light1',
     new BABYLON.Vector3(0, 1, 0),
     scene
   );
+
   // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
   const sphere = BABYLON.Mesh.CreateSphere(
     'sphere1',
@@ -58,6 +65,7 @@ const createScene = (canvas: any, engine: any): any => {
     false,
     BABYLON.Mesh.FRONTSIDE
   );
+
   // Move the sphere upward 1/2 of its height
   sphere.position.y = 0.25;
 
