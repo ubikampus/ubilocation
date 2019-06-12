a5ad7a0a9a9458a155204b801b8dfdd6435fcc05
je-l <je-l@users.noreply.github.com>
2019-06-12 11:59:47 +0300

    Refactor majority of codebase to use ES6 classes

--- src/mqttConnection.ts
+++ src/mqttConnection.ts
@@ -1,20 +1,33 @@
-import { default as UbiMqtt } from 'ubimqtt';
-import { MqttMessage, deserializeMessage } from './mqttDeserialize';
+import MqttParser, { MqttMessage } from './mqttDeserialize';
 
 const MOCK_MESSAGE_INTERVAL = 2000;
 
-export const startMessageMocker = (
-  beaconId: string,
-  onMessage: (a: MqttMessage) => void,
-  interval = MOCK_MESSAGE_INTERVAL
-): NodeJS.Timeout => {
-  return setInterval(() => {
+export class FakeMqttGenerator {
+  intervalRef: NodeJS.Timeout;
+  onMessage: (a: MqttMessage) => void;
+  beaconId: string;
+  mqttParser: MqttParser;
+
+  constructor(
+    beaconId: string,
+    mqttParser: MqttParser,
+    onMessage: (a: MqttMessage) => void,
+    interval: number = MOCK_MESSAGE_INTERVAL
+  ) {
+    this.onMessage = onMessage;
+    console.log('generating mock messages...');
+    this.intervalRef = setInterval(this.generateMessage, interval);
+    this.beaconId = beaconId;
+    this.mqttParser = mqttParser;
+  }
+
+  generateMessage = () => {
     // TODO: set 3d model maximum length as possible upper limit
     const x = Math.floor((Math.random() * 1024) / 2);
     const y = Math.floor((Math.random() * 768) / 2);
 
     const messageStr = JSON.stringify({
-      beaconId,
+      beaconId: this.beaconId,
       x,
       y,
       z: 0,
@@ -25,7 +38,13 @@ export const startMessageMocker = (
       zr: 0.2,
       alignment: 123,
     });
-    const message = deserializeMessage(messageStr);
-    onMessage(message);
-  }, interval);
-};
+
+    const message = this.mqttParser.deserializeMessage(messageStr);
+    this.onMessage(message);
+  };
+
+  stop() {
+    clearInterval(this.intervalRef);
+    console.log('stopped generating mock messages');
+  }
+}
--- test/mqttConnectionTest.ts
+++ test/mqttConnectionTest.ts
@@ -1,13 +1,14 @@
-import { startMessageMocker } from '../src/mqttConnection';
-
+import { FakeMqttGenerator } from '../src/mqttConnection';
+import MqttParser from '../src/mqttDeserialize';
 
 describe('mqtt message generator', () => {
   it('should call onMessage with a deserialized message', done => {
-    const id = startMessageMocker(
-      'abc',
-      message => {
-        expect(message.beaconId).toBe('abc');
-        clearInterval(id);
+    const generator = new FakeMqttGenerator(
+      'id',
+      new MqttParser(),
+      msg => {
+        expect(msg.beaconId).toBe('id');
+        generator.stop();
         done();
       },
       0
