9c728f402d73ce871d04f1bb4ea99d790cec61f4
je-l <je-l@users.noreply.github.com>
2019-07-16 00:14:32 +0300

    Refactor Button and Modal components
    Restructure the code so that these components are reusable.
    
    Also use the new Modal component for QR code modal.

---
+++ client/src/visualization/qrCodeModal.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import QRcode from 'qrcode.react';
+
+import styled from 'styled-components';
+import Modal from './modal';
+
+interface Props {
+  modalIsOpen: boolean;
+  closeModal(): void;
+  modalText: string;
+}
+
+const QrRow = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const PaddedQrCode = styled(QRcode)`
+  margin: 20px;
+`;
+
+const QRcodeLink = styled.a`
+  word-break: break-all;
+`;
+
+const QrCodeModal = ({ modalIsOpen, closeModal, modalText }: Props) => (
+  <Modal
+    isOpen={modalIsOpen}
+    onRequestClose={closeModal}
+    contentLabel="QR-code"
+  >
+    <QRcodeLink href={modalText}>{modalText}</QRcodeLink>
+    <QrRow>
+      <PaddedQrCode value={modalText} size={256} />
+    </QrRow>
+  </Modal>
+);
+
+export default QrCodeModal;
