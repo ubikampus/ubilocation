37ab0559df72bdf31ccc3a419877963cc7b9954f
Matti Riekkinen <matti.riekkinen@icloud.com>
2019-07-17 18:03:28 +0300

    Add tests for the login check middleware

--- auth-server/test/loginTest.ts
+++ auth-server/test/loginTest.ts
@@ -1,20 +1,62 @@
 import supertest from 'supertest';
+import jwt from 'jsonwebtoken';
 import app from '../src/app';
 
 const api = supertest(app);
 
-test('login successful with valid credentials', async () => {
-  await api
-    .post('/login')
-    .send({ username: 'admin', password: '#Apollo11-July1969' })
-    .expect('Content-Type', /json/)
-    .expect(200);
+describe('tests for the login router', () => {
+  test('login successful with valid credentials', async () => {
+    await api
+      .post('/login')
+      .send({ username: 'admin', password: '#Apollo11-July1969' })
+      .expect('Content-Type', /json/)
+      .expect(200);
+  });
+
+  test('login fails with invalid credentials', async () => {
+    await api
+      .post('/login')
+      .send({ username: 'admin', password: 'InvalidPassword' })
+      .expect('Content-Type', /json/)
+      .expect(401);
+  });
 });
 
-test('login fails with invalid credentials', async () => {
-  await api
-    .post('/login')
-    .send({ username: 'admin', password: 'InvalidPassword' })
-    .expect('Content-Type', /json/)
-    .expect(401);
+describe('tests for the login check middleware', () => {
+  test('access denied if token missing', async () => {
+    await api
+      .post('/sign')
+      .expect('Content-Type', /json/)
+      .expect(401);
+  });
+
+  test('access denied if token invalid', async () => {
+    await api
+      .post('/sign')
+      .set('Authorization', 'Bearer invalidToken')
+      .expect('Content-Type', /json/)
+      .expect(401);
+  });
+
+  test('access denied if token contains no username', async () => {
+    const payload = {};
+    const token = jwt.sign(payload, process.env.SECRET || '');
+
+    await api
+      .post('/sign')
+      .set('Authorization', 'Bearer ' + token)
+      .expect('Content-Type', /json/)
+      .expect(401);
+  });
+
+  test('access denied if username not admin', async () => {
+    const payload = { username: 'notAdmin' };
+    const token = jwt.sign(payload, process.env.SECRET || '');
+
+    await api
+      .post('/sign')
+      .set('Authorization', 'Bearer ' + token)
+      .expect('Content-Type', /json/)
+      .expect(401);
+  });
 });
