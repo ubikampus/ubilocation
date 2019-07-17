651a8d845d8a9c76afd9fcadd966b6af3b3468b1
Matti Riekkinen <matti.riekkinen@icloud.com>
2019-07-17 13:41:31 +0300

    Minor edits

--- auth-server/src/controllers/login.ts
+++ auth-server/src/controllers/login.ts
@@ -1,18 +1,19 @@
-const jwt = require('jsonwebtoken')
-const loginRouter = require('express').Router()
+const jwt = require('jsonwebtoken');
+import { Request, Response } from 'express';
+const loginRouter = require('express').Router();
 
 const adminUsername = 'admin';
 const adminPassword = 'July1969#Apollo11';
 
-loginRouter.post('/', (request: any, response: any) => {
+loginRouter.post('/', (request: Request, response: Response) => {
   const body = request.body;
 
   if (!(body.username === adminUsername && body.password === adminPassword)) {
-    return response.status(401).json({ error: 'invalid username or password'});
+    return response.status(401).json({ error: 'invalid username or password' });
   }
 
   const userForToken = {
-    username: body.username
+    username: body.username,
   };
 
   const token = jwt.sign(userForToken, process.env.SECRET);
@@ -20,6 +21,6 @@ loginRouter.post('/', (request: any, response: any) => {
   response
     .status(200)
     .send({ token, username: body.username })
-})
+});
 
 module.exports = loginRouter;
