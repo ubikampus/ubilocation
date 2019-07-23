3658179ab5bbb6eb3013817fd057164fd0838212
je-l <je-l@users.noreply.github.com>
2019-07-23 21:09:16 +0300

    Refactor separate NavBar component
    Also fix bug where sidebar was left open when changing tabs.

---
+++ client/src/common/navBar.tsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  NavLink,
+  Link,
+  withRouter,
+  RouteComponentProps,
+} from 'react-router-dom';
+import { TiCog } from 'react-icons/ti';
+import btlogo from '../../asset/bluetooth_logo.svg';
+
+const AdminCog = styled.div`
+  color: white;
+  height: auto;
+  width: 30px;
+
+  & > svg {
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const NavContainer = styled.nav`
+  height: 48px;
+  width: 100%;
+  display: flex;
+  z-index: 1000;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: #4287f5;
+  font-family: 'Comfortaa', cursive;
+  font-size: 13px;
+`;
+
+const Logo = styled.div`
+  height: 25px;
+  width: 25px;
+  margin-left: 1em;
+  margin-right: 1em;
+  background-image: url("${btlogo}");
+`;
+
+const Items = styled.ul`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  list-style-type: none;
+`;
+
+const LinkBox = styled(NavLink)`
+  &.active {
+    > li {
+      color: #ffffff;
+      border-bottom: 3px solid #ffffff;
+    }
+  }
+`;
+
+const Content = styled.li`
+  text-align: center;
+  padding: 15px;
+  color: #bed4f7;
+  border-top: 3px solid transparent;
+  border-bottom: 3px solid transparent;
+  &:active {
+    color: #ffffff;
+    border-bottom: 3px solid #ffffff;
+  }
+  &:hover {
+    color: #ffffff;
+  }
+`;
+
+const Search = styled.input`
+  padding: 0.5em;
+  margin: 1.5em;
+  color: #ffffff;
+  border: none;
+  border-radius: 20px;
+  background: #68a0fc;
+  text-align: left;
+  ::placeholder {
+    color: #ffffff;
+    padding-left: 1em;
+    padding-right: 1em;
+    font-size: 12px;
+    opacity: 1;
+  }
+`;
+
+const AdminChip = styled.div`
+  font-size: 11px;
+  margin-right: 5px;
+  white-space: pre;
+  font-weight: 700;
+  color: white;
+`;
+
+const SidepanelButton = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  margin-right: 15px;
+`;
+
+interface Props {
+  isAdmin: boolean;
+  setCalibrationPanelOpen(a: boolean): void;
+  calibrationPanelOpen: boolean;
+}
+
+const NavBar = ({
+  isAdmin,
+  calibrationPanelOpen,
+  setCalibrationPanelOpen,
+  location: { pathname },
+  history,
+}: Props & RouteComponentProps) => (
+  <NavContainer>
+    <Items>
+      <Logo />
+      <LinkBox to="/" exact>
+        <Content>Map</Content>
+      </LinkBox>
+      <LinkBox to="/config">
+        <Content>Settings</Content>
+      </LinkBox>
+      <LinkBox to="/about">
+        <Content>About</Content>
+      </LinkBox>
+    </Items>
+    <Search placeholder="Search .." />
+    {isAdmin && (
+      <SidepanelButton
+        onClick={() => {
+          if (pathname !== '/') {
+            history.push('/');
+            setCalibrationPanelOpen(true);
+          } else {
+            setCalibrationPanelOpen(!calibrationPanelOpen);
+          }
+        }}
+      >
+        <AdminChip>Admin panel</AdminChip>
+        <AdminCog>
+          <TiCog />
+        </AdminCog>
+      </SidepanelButton>
+    )}
+  </NavContainer>
+);
+
+export default withRouter(NavBar);
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
-import { TiCog } from 'react-icons/ti';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
-import btlogo from '../asset/bluetooth_logo.svg';
 import AboutContainer from './aboutContainer';
 import {
   GenuineBusContainer,
@@ -15,6 +13,7 @@ import CalibrationPanel, {
   RaspberryLocation,
 } from './visualization/calibrationPanel';
 import { Location } from './common/typeUtil';
+import NavBar from './common/navBar';
 
 const NotFound = () => <h3>404 page not found</h3>;
 
@@ -24,104 +23,9 @@ const Fullscreen = styled.div`
   flex-direction: column;
 `;
 
-const NavContainer = styled.nav`
-  height: 48px;
-  width: 100%;
-  display: flex;
-  z-index: 1000;
-  align-items: center;
-  justify-content: flex-start;
-  background-color: #4287f5;
-  font-family: 'Comfortaa', cursive;
-  font-size: 13px;
-`;
-
-const Logo = styled.div`
-  height: 25px;
-  width: 25px;
-  margin-left: 1em;
-  margin-right: 1em;
-  background-image: url("${btlogo}");
-`;
-
-const Items = styled.ul`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  list-style-type: none;
-`;
-
-const LinkBox = styled(NavLink)`
-  &.active {
-    > li {
-      color: #ffffff;
-      border-bottom: 3px solid #ffffff;
-    }
-  }
-`;
-
-const Content = styled.li`
-  text-align: center;
-  padding: 15px;
-  color: #bed4f7;
-  border-top: 3px solid transparent;
-  border-bottom: 3px solid transparent;
-  &:active {
-    color: #ffffff;
-    border-bottom: 3px solid #ffffff;
-  }
-  &:hover {
-    color: #ffffff;
-  }
-`;
-
-const Search = styled.input`
-  padding: 0.5em;
-  margin: 1.5em;
-  color: #ffffff;
-  border: none;
-  border-radius: 20px;
-  background: #68a0fc;
-  text-align: left;
-  ::placeholder {
-    color: #ffffff;
-    padding-left: 1em;
-    padding-right: 1em;
-    font-size: 12px;
-    opacity: 1;
-  }
-`;
-
-const AdminChip = styled.div`
-  font-size: 11px;
-  margin-right: 5px;
-  white-space: pre;
-  font-weight: 700;
-  color: white;
-`;
-
 const MainRow = styled.div`
   display: flex;
   height: 100%;
-`;
-
-const AdminCog = styled.div`
-  color: white;
-  height: auto;
-  width: 30px;
-
-  & > svg {
-    height: 100%;
-    width: 100%;
-  }
-  /* margin-right: 15px; */
-`;
-
-const SidepanelButton = styled.div`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  margin-right: 15px;
 `;
 
 const Router = () => {
@@ -140,57 +44,44 @@ const Router = () => {
   return (
     <BrowserRouter basename={apiRoot()}>
       <Fullscreen>
-        <NavContainer>
-          <Items>
-            <Logo />
-            <LinkBox to="/" exact>
-              <Content>Map</Content>
-            </LinkBox>
-            <LinkBox to="/config">
-              <Content>Settings</Content>
-            </LinkBox>
-            <LinkBox to="/about">
-              <Content>About</Content>
-            </LinkBox>
-          </Items>
-          <Search placeholder="Search .." />
-          {isAdmin && (
-            <SidepanelButton
-              onClick={() => setCalibrationPanelOpen(!calibrationPanelOpen)}
-            >
-              <AdminChip>Admin panel</AdminChip>
-              <AdminCog>
-                <TiCog />
-              </AdminCog>
-            </SidepanelButton>
-          )}
-        </NavContainer>
-
+        <NavBar
+          isAdmin={isAdmin}
+          setCalibrationPanelOpen={setCalibrationPanelOpen}
+          calibrationPanelOpen={calibrationPanelOpen}
+        />
         <MainRow>
-          {calibrationPanelOpen && (
-            <CalibrationPanel
-              newHeight={newHeight}
-              setNewHeight={setNewHeight}
-              newName={newName}
-              onLogout={() => {
-                setIsAdmin(false); // TODO: actually logout
-                setCalibrationPanelOpen(false);
-              }}
-              setNewName={setNewName}
-              onSubmit={devices => {
-                console.log('TODO: sent to mqtt bus after signing the message');
-              }}
-              onCancel={() => {
-                setCalibrationPanelOpen(false);
-                setRaspberryLocation(null);
-                setRaspberryDevies([]);
-              }}
-              raspberryDevices={raspberryDevices}
-              setRaspberryDevices={setRaspberryDevies}
-              raspberryLocation={raspberryLocation}
-              resetRaspberryLocation={() => setRaspberryLocation(null)}
-            />
-          )}
+          <Route
+            exact
+            path="/"
+            render={() =>
+              calibrationPanelOpen && (
+                <CalibrationPanel
+                  newHeight={newHeight}
+                  setNewHeight={setNewHeight}
+                  newName={newName}
+                  onLogout={() => {
+                    setIsAdmin(false); // TODO: actually logout
+                    setCalibrationPanelOpen(false);
+                  }}
+                  setNewName={setNewName}
+                  onSubmit={devices => {
+                    console.log(
+                      'TODO: sent to mqtt bus after signing the message'
+                    );
+                  }}
+                  onCancel={() => {
+                    setCalibrationPanelOpen(false);
+                    setRaspberryLocation(null);
+                    setRaspberryDevies([]);
+                  }}
+                  raspberryDevices={raspberryDevices}
+                  setRaspberryDevices={setRaspberryDevies}
+                  raspberryLocation={raspberryLocation}
+                  resetRaspberryLocation={() => setRaspberryLocation(null)}
+                />
+              )
+            }
+          />
 
           <Switch>
             <Route
