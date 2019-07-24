0c5ae63b2c3be5d6797c1c810dd60bca4e3bc12b
Matti Riekkinen <matti.riekkinen@icloud.com>
2019-07-24 14:27:54 +0300

    Add better styles for the login form

--- client/src/visualization/loginPrompt.tsx
+++ client/src/visualization/loginPrompt.tsx
@@ -1,6 +1,59 @@
 import React, { ChangeEvent, FormEvent } from 'react';
 import { withRouter, RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+/**
+ * Credit: The CSS styles on this page are based on
+ * (1) https://colorlib.com/wp/template/creative-login-form/
+ * (2) the WordPress login form.
+ */
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #f2f2f2;
+`;
+
+const LoginPage = styled.div`
+  margin: auto;
+  width: 320px;
+  padding: 45px 0;
+  text-align: center;
+`;
+
+const LoginHeader = styled.h1`
+  font-size: 24px;
+`;
+
+const LoginForm = styled.form`
+  padding: 26px 24px 26px;
+  margin: 15px 0 20px;
+  background: white;
+  max-width: 320px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.13);
+`;
+
+const FormInput = styled.input`
+  width: 100%;
+  padding: 5px;
+  box-sizing: border-box;
+  margin: 0 0 10px;
+  font-size: 14px;
+  background: #fbfbfb;
+  border: 1px solid #ddd;
+`;
+
+const FormButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  border: 0;
+  font-size: 14px;
+  color: white;
+  background: #5599ff;
+  &:hover {
+    background: #2a7fff;
+  }
+`;
 
 interface Props {
   username: string;
@@ -17,33 +70,29 @@ const LoginPrompt = ({
   setPassword,
   onSubmit,
 }: RouteComponentProps & Props) => (
-  <div>
-    <h3>Admin Login</h3>
-    <form onSubmit={onSubmit}>
-      <p>
-        <input
+  <Background>
+    <LoginPage>
+      <LoginHeader>Admin Login</LoginHeader>
+      <LoginForm onSubmit={onSubmit}>
+        <FormInput
           type="text"
           placeholder="Username"
           value={username}
           onChange={setUsername}
         />
-      </p>
-      <p>
-        <input
+        <FormInput
           type="password"
           placeholder="Password"
           value={password}
           onChange={setPassword}
         />
-      </p>
+        <FormButton type="submit">Log In</FormButton>
+      </LoginForm>
       <p>
-        <button type="submit">Log In</button>
+        <Link to="/">&larr; Back to the map</Link>
       </p>
-    </form>
-    <p>
-      <Link to="/">Go back to the map</Link>
-    </p>
-  </div>
+    </LoginPage>
+  </Background>
 );
 
 export default withRouter(LoginPrompt);
