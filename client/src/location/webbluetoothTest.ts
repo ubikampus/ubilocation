b2cac929ba894a073e7db1242bc6736c5cce4d1a
je-l <je-l@users.noreply.github.com>
2019-07-28 17:46:33 +0300

    Add module with bluetooth name fetching

---
+++ client/src/location/webbluetoothTest.ts
@@ -0,0 +1,41 @@
+import { TextDecoder, TextEncoder } from 'util';
+
+import fetchName from './webbluetooth';
+
+describe('web bluetooth', () => {
+  it('fetches bluetooth name successfully', async () => {
+    (global as any).TextDecoder = TextDecoder;
+    const encoder = new TextEncoder();
+    const btName = encoder.encode('android-123');
+
+    // nice
+    (navigator.bluetooth as any) = {
+      requestDevice: () =>
+        Promise.resolve({
+          gatt: {
+            connect: () =>
+              Promise.resolve({
+                getPrimaryService: () =>
+                  Promise.resolve({
+                    getCharacteristic: () =>
+                      Promise.resolve({
+                        getDescriptor: () =>
+                          Promise.resolve({
+                            readValue: () => Promise.resolve(btName),
+                          }),
+                      }),
+                  }),
+              }),
+          },
+        }),
+    };
+
+    const name = await fetchName();
+    expect(name).toBe('android-123');
+  });
+
+  it('returns empty if device doesnt support bluetooth', async () => {
+    (navigator.bluetooth as any) = undefined;
+    expect(await fetchName()).toEqual(null);
+  });
+});
