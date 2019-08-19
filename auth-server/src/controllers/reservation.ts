8c5080f1a1a9d40622283df75c96839b0c41fa8d
atte <atte.h.1998@gmail.com>
2019-08-19 16:14:52 +0300

    Recieves real data

--- auth-server/src/controllers/reservation.ts
+++ auth-server/src/controllers/reservation.ts
@@ -1,20 +1,40 @@
 import { Router } from 'express';
 import MqttService from '../services/mqtt';
 
-const reservations: Reservation[] = [];
 const url = process.env.MQTT_URL || 'mqtt://localhost';
 const topic = process.env.RESERVATION_TOPIC || 'temp';
 
 interface Reservation {
-  room: string;
-  time: string;
+  startTime: Date;
+  endTime: Date;
 }
 
+interface Reservations {
+  [room: string]: Reservation[];
+}
+
+let reservations: Reservations = {};
+
 const listener = (_: string, message: string) => {
+  const newReservations: Reservations = {};
+  const now = new Date();
+  for (const item of Object.entries(reservations)) {
+    const filtered = item[1].filter(i => i.endTime > now);
+    newReservations[item[0]] = filtered;
+  }
+
+  reservations = newReservations;
   try {
-    const parsed: Reservation = JSON.parse(message);
-    reservations.push(parsed)
-    console.log(reservations);
+    const parsed = JSON.parse(message);
+
+    if (!reservations[parsed.nextId]) {
+      reservations[parsed.nextId] = [];
+    }
+
+    reservations[parsed.nextId].push({
+      startTime: parsed.nextStartTime,
+      endTime: parsed.nextEndTime,
+    });
   } catch (err) {
     console.error(err);
   }
@@ -24,8 +44,7 @@ new MqttService().connect(url).then(s => s.subscribe(topic, listener));
 
 const reservationRouter = Router();
 
-reservationRouter.get('/', (request, response) => {
-  console.log(reservations);
+reservationRouter.get('/', (_, response) => {
   response.json(reservations);
 });
 
