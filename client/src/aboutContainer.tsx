fbccf7a64b767d114f1bd58292771838b00b56a4
Elizabeth Berg <elizabethinposti@gmail.com>
2019-08-27 20:19:44 +0300

    Add footer

--- client/src/aboutContainer.tsx
+++ client/src/aboutContainer.tsx
@@ -7,12 +7,11 @@ import { PrimaryButton } from '../src/common/button';
 /** Container */
 const About = styled.div`
   max-width: 800px;
-
   display: flex;
   flex-direction: column;
   justify-content: center;
   margin: auto;
-  padding: 45px 0;
+  padding: 40px 0 0;
 
   @media (max-width: 840px) {
     margin: auto 2rem;
@@ -20,6 +19,7 @@ const About = styled.div`
 `;
 
 const Wrap = styled.div`
+  width: 100vw;
   margin: auto;
 `;
 
@@ -63,13 +63,11 @@ const Logo = styled.img`
   height: 50%;
 `;
 
-const Divider = styled.div`
-  padding-top: 20px;
-  border-bottom: 1px solid lightgray;
-`;
-
 /** Content */
 const Content = styled.div`
+  flex: 1 0 auto;
+  padding-bottom: 25px;
+
   text-align: left;
   line-height: 1.6;
 
@@ -106,6 +104,7 @@ const SplitContent = styled.div`
   }
 `;
 
+/** Align text to center on mobile */
 const ContentItem = styled.div`
   & > * {
     @media (max-width: 575px) {
@@ -119,18 +118,18 @@ const ContentHeadline = styled.h2`
   align-items: center;
   height: 33px;
   padding: 10px 0;
-  border-radius: 10px;
   padding: 0 15px;
 
   font-size: 16px;
   font-weight: 700;
-  text-transform: uppercase;
   font-family: 'Comfortaa', cursive;
-  background-color: salmon;
+  text-transform: uppercase;
+  border-bottom: 5px solid salmon;
 
-  @media (max-width: 575px) {
-    justify-content: center;
+  /** Align headlin text to center on mobile */
+  @media (max-width: 575px) { 
     text-align: center;
+    justify-content: center;
   }
 `;
 
@@ -141,18 +140,27 @@ const ContentText = styled.div`
   font-family: 'Raleway', sans-serif;
 `;
 
+const ContentTextBold = styled(ContentText)`
+  font-weight: 700;
+`;
+
 /** Footer */
 const Footer = styled.footer`
   display: flex;
-  margin: 0;
-  justify-content: center;
+  padding: 10px;
+  height: 50px;
   width: 100%;
-  height: 40px;
-  background-color: #20262b;
+  align-items: center;
+  justify-content: center;
+
   color: white;
   font-size: 13px;
   text-align: center;
+  font-family: inherit;
+  background-color: #20262b;
 `;
+
+const FooterText = styled.div``;
 
 const AboutContainer = () => (
   <>
@@ -174,54 +182,57 @@ const AboutContainer = () => (
               course Software Engineering Lab at the University of Helsinki,
               department of Computer Science. The aim of the project is to
               calculate campus visitors’ indoor position by using an inverse
-              technique of collecting Bluetooth signals via minicomputers such
-              as Raspberry PIs placed on the walls. Ubilocation is part of the
-              Ubikampus project and is built on the specifications and
-              requirements of Ubikampus’ coordinator Petri Savolainen.
-              <br />
-              <br />
+              technique of collecting Bluetooth signals via minicomputers (in
+              this case Android devices) placed on the floors, tables or walls.
+              Ubilocation is part of the Ubikampus project and is built on the
+              specifications and requirements of Ubikampus’ coordinator Petri
+              Savolainen.
+            </ContentText>
+            <ContentTextBold>
               Please note that users need to carry Bluetooth beacons with them
               (the size of small, electronic keys) or download a beacon
               simulation app in order to be positioned.
-            </ContentText>
+            </ContentTextBold>
           </ContentItem>
 
           <SplitContent>
             <ContentItem>
               <ContentHeadline>Contributing</ContentHeadline>
               <ContentText>
-                Ubilocation is an Open Source project consisting of three
-                code repositories: Android scanner, Ubilocation
-                library and an user application. If you would like to develop the
-                project further please visit Ubilocation’s GitHub page and
-                submit an issue and/or pull request in one of the repositories.
-                <br />
-                <br />
-                Android Scanner
-                <br />
-                Ubilocation Library
-                <br />
-                Ubilocation User Application
+                Ubilocation is an Open Source project consisting of five code
+                repositories: Android scanner, Indoor Positioning library, Room
+                reservator, Ubilocation server and Ubilocation client. If you
+                would like to develop the project further please visit
+                Ubilocation’s GitHub page and submit an issue and/or pull
+                request in one of the repositories.
               </ContentText>
             </ContentItem>
 
             <ContentItem>
               <ContentHeadline>Creators</ContentHeadline>
               <ContentText>
-                Elizabeth Berg, UI/UX<br />
-                Jere Lahelma, front-end<br />
-                Matti Riekkinen, front-end<br />
-                Atte Haarni, full-stack<br />
-                Joni Kokko, back-end<br />
-                Emil Andersson, back-end<br />
-                Aleksander Matikainen, back-end<br />
+                Elizabeth Berg, UI/UX
+                <br />
+                Jere Lahelma, front-end
+                <br />
+                Matti Riekkinen, front-end
+                <br />
+                Atte Haarni, full-stack
+                <br />
+                Joni Kokko, back-end
+                <br />
+                Emil Andersson, back-end
+                <br />
+                Aleksander Matikainen, back-end
+                <br />
               </ContentText>
             </ContentItem>
           </SplitContent>
         </Content>
       </About>
-
-      <Footer>Helsingin yliopisto 2019</Footer>
+      <Footer>
+        <FooterText>&#169; University of Helsinki 2019</FooterText>
+      </Footer>
     </Wrap>
   </>
 );
